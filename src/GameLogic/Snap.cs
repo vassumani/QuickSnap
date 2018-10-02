cbf8f6ee247e2245bd18edcf8a63e0ecbb07ee09
Zoran <zorang@live.com.au>
2018-10-02 19:47:39 +1000

    error

--- src/GameLogic/Snap.cs
+++ src/GameLogic/Snap.cs
@@ -153,7 +153,7 @@ namespace CardGames.GameLogic
             }
 			// stop the game...
 			_started = false;
-            _gameTimer.Stop();
+           
         }
 	
 		#region Snap Game Unit Tests
